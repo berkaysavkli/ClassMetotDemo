d64219b1ea76339171af48500c9c9273b6d91d5d
BERKAY ŞAVKLI <berkaysavkli@gmail.com>
2021-01-19 00:12:18 +0300

    18.01.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler " + musteri.Adi +" "+ musteri.Soyadi + " Bankamıza kaydınız tamamlandı !");
+        }
+            
+            
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,34 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri Musteri1 = new Musteri();
+            Musteri1.MusteriNumarasi = 11111;
+            Musteri1.Adi = "Berkay";
+            Musteri1.Soyadi = "ŞAVKLI";
+            Musteri1.DogumYili = 1995;
+
+            Musteri Musteri2 = new Musteri();
+            Musteri2.MusteriNumarasi = 11112;
+            Musteri2.Adi = "Nilay";
+            Musteri2.Soyadi = "ŞAVKLI";
+            Musteri2.DogumYili = 2001;
+
+            Musteri[] musteriler = new Musteri[] { Musteri1,Musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri ID : " + musteri.MusteriNumarasi);
+                Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı : " + musteri.Soyadi);
+                Console.WriteLine("Müşteri Doğum Yılı : " + musteri.DogumYili);
+                Console.WriteLine("--------------------------------------");
+            }
+
+            MusteriManager musteriManager =new MusteriManager();
+            musteriManager.Ekle(Musteri1);
+            musteriManager.Ekle(Musteri2);
+                
+
         }
     }
 }
